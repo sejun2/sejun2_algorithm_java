e68b086f35b8589c341437fde73f52d8a3c4f1f6
sejun <qpfjf56@gmail.com>
2025-12-04 02:19:38 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 14.22 ms, Memory: 55.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,71 +1,82 @@
 import java.util.*;
+import java.math.*;
 
 class Solution {
+    int answer = Integer.MAX_VALUE;
+    int[] dx = new int[] {-1, 0, 1, 0};
+    int[] dy = new int[] {0, 1, 0, -1};
+    int step = 1;
+    boolean reached = false;
+    
     public int solution(int[][] maps) {
-        int xLen = maps.length;
-        int yLen = maps[0].length;
-
-        boolean[][] visited = new boolean[xLen][yLen];
-
-        return bfs(visited, new OffsetNode(0,0), maps);
+        boolean[][] visited = new boolean[maps.length][maps[0].length];
+        
+        // dfs(visited, maps, 0,0, 0);
+        bfs(visited, maps, 0, 0);
+       
+        if(!reached) return -1;
+        
+        return step;
+        
+        //if(answer == Integer.MAX_VALUE) return -1;
+        
+        //return answer;
     }
-
-    int bfs(boolean[][] visited, OffsetNode offset, int[][] maps){
-        Queue<OffsetNode> q = new LinkedList<OffsetNode>();
-        int[][] steps = new int[maps.length][maps[0].length];
-
-
-        q.add(offset);
-        visited[offset.x][offset.y]  = true;
-        steps[offset.x][offset.y]  = 1;
-
+    
+    void bfs(boolean[][] visited, int[][] maps, int currentX, int currentY){
+       Queue<Integer[]> q = new LinkedList<>(); 
+        
+       q.add(new Integer[]{currentX, currentY, 1});
+       visited[currentX][currentY] = true;
+        
         while(!q.isEmpty()){
-
-            OffsetNode pollNode = q.poll();
-
-
-            if(pollNode.x == maps.length-1 && pollNode.y == maps[0].length-1){
-                return steps[pollNode.x][pollNode.y];
+            Integer[] axis = q.poll();
+            
+            if(axis[0] == maps.length - 1 && axis[1] == maps[0].length - 1){
+                // 목적지 도달
+                reached = true;
+                step = axis[2];
+                return;
             }
-
-            // west
-            if(pollNode.y-1 >=0 && maps[pollNode.x][pollNode.y-1] == 1 && visited[pollNode.x][pollNode.y-1] != true){
-                q.add(new OffsetNode(pollNode.x, pollNode.y-1));
-                visited[pollNode.x][pollNode.y-1] = true;
-                steps[pollNode.x][pollNode.y-1] = steps[pollNode.x][pollNode.y] + 1;
+            
+            for(int i =0 ;i<4; i++){
+                int nextX = axis[0] + dx[i];
+                int nextY = axis[1] + dy[i];
+                int nextStep = axis[2] + 1;
+                
+                if(nextX < 0 || nextX >= maps.length || nextY < 0 || nextY >= maps[0].length) continue;
+                
+                if(maps[nextX][nextY] == 0) continue;
+                
+                if(visited[nextX][nextY]) continue;
+                
+       			visited[nextX][nextY] = true;
+                q.add(new Integer[] {nextX, nextY, nextStep});
             }
-            // north
-            if(pollNode.x -1 >=0 && maps[pollNode.x-1][pollNode.y] == 1 && visited[pollNode.x-1][pollNode.y] != true){
-                q.add(new OffsetNode(pollNode.x-1, pollNode.y));
-                visited[pollNode.x-1][pollNode.y] = true;
-                steps[pollNode.x-1][pollNode.y] = steps[pollNode.x][pollNode.y] + 1;
-            }
-
-            // south
-            if(pollNode.x+1 < maps.length && maps[pollNode.x+1][pollNode.y] == 1 && visited[pollNode.x+1][pollNode.y] != true){
-                q.add(new OffsetNode(pollNode.x+1, pollNode.y));
-                visited[pollNode.x+1][pollNode.y] = true;
-                steps[pollNode.x+1][pollNode.y] = steps[pollNode.x][pollNode.y] + 1;
-            }
-
-            // east
-            if(pollNode.y+1 < maps[0].length && maps[pollNode.x][pollNode.y+1] == 1 && visited[pollNode.x][pollNode.y+1] != true){
-                q.add(new OffsetNode(pollNode.x, pollNode.y+1));
-                visited[pollNode.x][pollNode.y+1] = true;
-                steps[pollNode.x][pollNode.y+1] = steps[pollNode.x][pollNode.y] + 1;
-            }
-
         }
-
-        return -1;
     }
-
-}
-class OffsetNode{
-    int x, y;
-
-    OffsetNode(int x, int y){
-        this.x = x;
-        this.y = y;
+    
+    void dfs(boolean[][] visited, int[][] maps, int currentX, int currentY,  int step){
+        if(currentX <0 || currentX > maps.length -1 || currentY > maps[0].length -1 || currentY < 0) return;
+        
+        if(maps[currentX][currentY] == 0) return;
+        
+        if(visited[currentX][currentY]) return;
+        
+       	if(currentX == maps.length-1 && currentY == maps[0].length-1) {
+            // 목적지 도달
+            answer = Math.min(answer, step+1);
+            return;
+        }
+        
+        visited[currentX][currentY]  = true;
+        step++;
+       
+        for(int i =0;i<4;i++){
+          dfs(visited, maps, currentX + dx[i], currentY + dy[i], step);
+        }
+        
+        visited[currentX][currentY]  = false;
+        step--;
     }
 }
